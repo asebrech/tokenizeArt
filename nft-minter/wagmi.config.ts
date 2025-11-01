31aaed6c00921c7795659b113ea94837624a1608
asebrech <alois.sebrechts@gmail.com>
2025-11-01 17:25:12 +0100

    rm wallet connect config

--- nft-minter/wagmi.config.ts
+++ nft-minter/wagmi.config.ts
@@ -1,16 +1,11 @@
-import { http, createConfig } from 'wagmi'
-import { sepolia } from 'wagmi/chains'
-import { injected, walletConnect } from 'wagmi/connectors'
+import { http, createConfig } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { injected } from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [sepolia],
-  connectors: [
-    injected(),
-    walletConnect({ 
-      projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '' 
-    }),
-  ],
+  connectors: [injected()],
   transports: {
     [sepolia.id]: http(),
   },
-})
+});
